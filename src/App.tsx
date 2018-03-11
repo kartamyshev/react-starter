ea427eedc4a1d2298a6896117dec339d6eed35d3
kartamyshev <kartamyshev.k@yandex.ru>
2018-03-11 07:50:38 +0100

    updated typings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Sample } from 'components/Sample';
+import { Sample } from './components/Sample/Sample';
 import { Fragment } from './utils/Fragment';
 import 'css/base.less';
 
 ReactDOM.render(
   <Fragment>
-    <Sample />
+    <Sample ololoev={false} name="Kostya" />
   </Fragment>,
   document.getElementById('application-container')
 );
